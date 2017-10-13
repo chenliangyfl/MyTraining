dc6c5f7bd174f8616bb5f2b18ff051ef138f46ef
chenliang <chenliang@rongshutong.com>
2017-10-13 11:16:23 +0800

    Fixed: 多线程爬数据存在服务器繁忙的问题

--- myspider.py
+++ myspider.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import re
+import os
+import codecs
 from multiprocessing import Process, Queue
 from Queue import Queue as _Que
-# from threading import Thread
+from threading import Thread
 import gevent
 from lxml import etree
 import time
 import requests
+import urllib2
 
 
 class DoubanSpider(Process):
@@ -105,12 +109,85 @@ class Dianshiju(object):
             print self.q.get()
 
 
+class DianshijuThread(Thread):
+    def __init__(self, url, name):
+        super(DianshijuThread, self).__init__()
+        self.url = url
+        self.dirname = name
+
+    def parse_page(self):
+        base_url = "http://www.dy2018.com"
+        html = self.send_request(self.url)
+        body = etree.HTML(html)
+        node_list = body.xpath("//div[@class='co_content8']//a")
+        url_list = []
+
+        for node in node_list:
+            url = node.xpath("@href")[0]
+            if url[1] != "i":
+                break
+            url_list.append(base_url + url)
+
+        for page_url in url_list:
+            page_html = self.send_request(page_url)
+            page_body = etree.HTML(page_html)
+            urls_list = page_body.xpath("//table[@style]//a/text()")
+
+            ftp_list = []
+            file_name = ""
+            if len(urls_list) > 0:
+                file_path = os.path.abspath("..\\..\\") + "\\" + self.dirname
+                if not os.path.exists(file_path):
+                    os.mkdir(file_path)
+                for u in urls_list:
+                    if "ftp" in u:
+                        if not file_name:
+                            file_name = u.split("/")[-1:][0].split(".")[0]
+                            file_name = "".join(re.findall(r'\D+', file_name))
+                            print file_name
+                        ftp_list.append(u)
+                ftp_list = [line + "\n" for line in ftp_list]
+                file_name = file_path + "\\" + file_name + ".txt"
+                print file_name
+                for line in ftp_list:
+                    with codecs.open(file_name, "a", "utf-8") as f:
+                        f.write(line)
+
+    def run(self):
+        self.parse_page()
+
+    @staticmethod
+    def send_request(url):
+        for _ in range(3):
+            try:
+                print u"[INFO]请求url:{}".format(url)
+                response = urllib2.urlopen(url)
+                page = response.read()
+                return page.decode('gbk', 'ignore')
+            except Exception as e:
+                print u"[ERROR]请求失败-->{}".format(e)
+
+
 if __name__ == "__main__":
     start = time.time()
 
     # main()
-    get_ftp = Dianshiju()
+    # get_ftp = Dianshiju()
+    #
+    # get_ftp.main(98338)
+    testurl = "http://www.dy2018.com/html/tv/hytv/index.html"
+    base_url = "http://www.dy2018.com/html/tv/hytv/index_{}.html"
+    url_list = [base_url.format(num) for num in range(2, 28)]
+    url_list.append(testurl)
 
-    get_ftp.main(98338)
+    thread_list = []
+
+    for url in url_list:
+        get_url = DianshijuThread(url, 'mytest')
+        get_url.start()
+        thread_list.append(get_url)
+
+    for p in thread_list:
+        p.join()
     print u"[INFO]耗时:{}".format(time.time() - start)
 
